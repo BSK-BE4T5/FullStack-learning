a8f6ee847d67ba2529756038af01f3403c71516e
bsk-be4t5 <panfilodemon1@gmail.com>
2022-05-11 16:44:51 -0600

    main

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -5,13 +5,14 @@ var audioTrack = WaveSurfer.create({
     progressColor: '#7A3AC4',
     barWidth: 1,
     responsive: true,
-    barHeight:60,
+    barHeight: 60,
     hideScrollbar: true,
 });
 
 audioTrack.load('Bf.wav');
 
 const playBtn = document.querySelector(".play-btn")
+const playBtn2 = document.querySelector(".play-btn2")
 
 playBtn.addEventListener("click",()=>{
     audioTrack.playPause();
@@ -28,3 +29,28 @@ playBtn.addEventListener("click",()=>{
 
 audioTrack.setVolume(1.5);
 
+var audioTrack2 = WaveSurfer.create({
+    container: '.audio2',
+    waveColor: '#fff',
+    progressColor: '#7A3AC4',
+    barWidth: 1,
+    responsive: true,
+    barHeight: 60,
+    hideScrollbar: true,
+});
+
+audioTrack2.load('Loop2.wav');
+playBtn2.addEventListener("click",()=>{
+    audioTrack2.playPause();
+
+    if (audioTrack2.isPlaying()){
+        playBtn2.classList.add("playing");
+        
+
+    }else{
+        playBtn2.classList.remove("playing");
+        
+    }
+})
+
+audioTrack2.setVolume(1.5);
